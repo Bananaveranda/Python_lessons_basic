cdc264ac54b3dad7e2aa6c4377be7b5e4475d4d6
Bananaveranda <51752513+Bananaveranda@users.noreply.github.com>
2019-06-28 10:26:01 +0300

    start hw2
    end easy , task 1, task 2

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -12,12 +12,41 @@
 # 4.  арбуз
 
 # Подсказка: воспользоваться методом .format()
-
-
+#python2
+fruits = ["яблоко", "банан", "киви", "арбуз"]
+for idx, itm in enumerate(fruits, start=1):
+    print('{}. {:>10}'.format(idx, itm))
+    
+    
+#python3
+fruits = ["яблоко", "банан", "киви", "арбуз"]
+max_len = len(max(fruits))
+i = 0
+while i <len(fruits):
+    print(f'{i+1}.{fruits[i],rjust(max_len," ")}')
+    i+=1
+    
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
 
+          import random
+list_1 = []
+for i in range(10):
+    element = random.randint(1,10)
+    list_1.append(element)
+list_2 = []
+for i in range(10):
+    element = random.randint(1,10)
+    list_2.append(element)
+new_list = []
+i = 0
+while i < len(list_2):
+    if not list_1[i] in list_2:
+        new_list.append(list_1[i])
+    else:
+        break
+print('{} уникальный список из {}, очищенный от списка {}'.format(new_list, list_1, list_2))
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
