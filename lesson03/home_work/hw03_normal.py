3b8b586838dc18aee21ff0e77fcdd79eca2fd66b
Bananaveranda <51752513+Bananaveranda@users.noreply.github.com>
2019-07-02 17:36:58 +0300

    Update hw03 normal
    Сделаны задача 1 и задача 2

--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -3,18 +3,28 @@
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    fibonacciRow = [1, 1]
+    for idx in range(2, m+1):
+        a = fibonacciRow[idx-1]+fibonacciRow[idx-2]
+        fibonacciRow.append(a)
+    fibonacciRow = fibonacciRow[n:m+1]
+    return fibonacciRow
+
+print(fibonacci(78,102)) #          счет чисел начинается с нуля
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
-
-def sort_to_max(origin_list):
-    pass
-
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+def sort_to_max(spisok):
+    for n in range(0, len(spisok)-1):
+        for i in range(0, len(spisok)-1-n):
+            if spisok[i] > spisok[i+1]:
+                spisok[i],spisok[i+1] = spisok[i+1],spisok[i]
+    return(spisok)
+a = sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+print(a)
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
